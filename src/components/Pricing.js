0c0c20dac9fab3e7654e413f416705e73c73dcc7
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-05-30 13:01:32 +0300

    feat: add pricing section

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+
+import { pricing } from "../data";
+
+import PlanList from "./PlanList";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  const { icon, title, plans } = pricing;
+
+  return (
+    <div className="section">
+      <div
+        className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        datat-aos-delay="200"
+      >
+        <img src={icon} alt="" />
+        <h2 className="h2">
+          {title}
+          <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+      <PlanList plans={plans} />
+    </div>
+  );
 };
 
 export default Pricing;
