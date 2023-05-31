0102feafa832077553d1ffbe0ce3bf2f2dc59520
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-05-31 18:18:19 +0300

    feat: add workouts section

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { workouts } from '../data';
+
+import WorkoutSlider from '../components/WorkoutSlider'
+
 const Workouts = () => {
-  return <div>Workouts</div>;
+  const { title, icon } = workouts
+
+  return (
+    <section className='section'>
+      <div
+        className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0'
+        data-aos='fade-up'
+        data-aos-delay='100'
+      >
+        <img src={icon} alt='' />
+        <h2 className='h2 section-title'>{ title }<span className='text-primary-200'>.</span></h2>
+      </div>
+      <div
+      className='lg:max-w-[1500px] mx-auto'
+        data-aos='fade-up'
+        data-aos-delay='150'
+      >
+        <WorkoutSlider />
+      </div>
+    </section>
+  )
 };
 
 export default Workouts;
