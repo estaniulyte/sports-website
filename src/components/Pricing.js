b64559730e248f36f4589c9c097d8a7744f28cb9
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-01 09:54:55 +0300

    remove: remove offset and delay for animation

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -12,8 +12,6 @@ const Pricing = () => {
       <div
         className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
         data-aos="fade-up"
-        data-aos-offset="200"
-        datat-aos-delay="200"
       >
         <img src={icon} alt="" />
         <h2 className="h2">
