cdd76c2e5ee4b5403f2667f058283afd321199f7
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-03 08:02:42 +0300

    feat: create community section

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+
+import { community } from "../data";
+
+import CommunitySlider from "../components/CommunitySlider";
 
 const Community = () => {
-  return <div>Community</div>;
+  const { icon, title, testimonials } = community;
+
+  return (
+    <section className="section relative lg:max-w-[1500px] lg:mx-auto">
+      <div className="container mx-auto">
+        <div className="flex">
+          <div className="section-title-group max-w-[240px] px-4 lg:px-0 lg:ml-0 mx-auto">
+            <img src={icon} alt="section icon" />
+            <h2 className="h2 section-title">
+              {title}
+              <span className="text-primary-200">.</span>
+            </h2>
+          </div>
+          <div className="absolute -right-[275px] lg:-right-[200px] w-[1000px] lg:w-[1140px] top-48 lg:top-0">
+            <CommunitySlider testimonials={testimonials} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Community;
